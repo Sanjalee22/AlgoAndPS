2369b081638bb70987820cbc018887985d2d64d6
Sanjalee22 <34329090+Sanjalee22@users.noreply.github.com>
2020-06-19 10:26:57 +0530

    Update Tree_LCAInBST.cs

--- Tree_LCAInBST.cs
+++ Tree_LCAInBST.cs
@@ -17,3 +17,4 @@ public void BST_LCA(Node n1, Node n2, Node root)
 
             Console.WriteLine(root.data);
         }
+//the node where n1 is at left side and n2 is at right side, that node is LCA.
