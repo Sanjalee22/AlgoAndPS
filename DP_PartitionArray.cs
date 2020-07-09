faf92059980b7efb9a763b40797a090896809ace
Sanjalee22 <34329090+Sanjalee22@users.noreply.github.com>
2020-07-09 09:24:03 +0530

    Create DP_PartitionArray.cs

---
+++ DP_PartitionArray.cs
@@ -0,0 +1,67 @@
+public bool CanPartition(int[] nums) {
+        
+        //calculate the sum
+        int n=nums.Length;
+        int sum=0;
+        for(int i=0;i<n;++i){
+            sum=sum+nums[i];
+        }
+         
+        //if sum is odd, return false
+        if(sum%2!=0)
+            return false;
+        
+        //get the half of sum
+        sum=sum/2;
+        
+        //sort the array nums
+        //Array.Sort(nums);
+        
+        //create a solution array 
+        bool[,] solution=new bool[n,sum+1];
+        
+        //set first column as true as it is possible to get zero sum with an empty set
+        for(int i=0;i<n;++i)
+            solution[i,0]=true;
+        
+        //set first row based on nums[0]
+        
+        for(int j=1;j<=sum;++j)
+        { 
+            if(j==nums[0])
+               solution[0,j]=true;
+            else
+                solution[0,j]=false;
+         }
+        
+        //now loop through all the possible sums for all the elements
+        
+        //for every element in nums
+        for(int i=1;i<n;++i)
+        {
+            for(int j=1;j<=sum;++j)
+            {
+                //if this column element is less that the current row element, set false
+                if(j<nums[i])
+                    solution[i,j]=solution[i-1,j];
+                else
+                {
+                    //if the current sum is possible with already included numbers
+                    if(solution[i-1,j])
+                        solution[i,j]=true;
+                    
+                    //else check if current sum - current num is true, then current sum is also
+                    //true if we include this number
+                    else
+                        solution[i,j]=solution[i-1,j-nums[i]];
+                }
+            }
+            
+            //return as soon as the sum is found possible. No need to loop further.
+            if(solution[i,sum])
+                return true;
+        }
+        
+        return solution[n-1,sum];
+        
+    }
