eba6ffcb178f2158e94bd56230cc774a61aa47ce
Sanjalee22 <34329090+Sanjalee22@users.noreply.github.com>
2020-08-18 19:41:46 +0530

    Update BinaryTree_SpiralLevelOrderTraversal.cs

--- BinaryTree_SpiralLevelOrderTraversal.cs
+++ BinaryTree_SpiralLevelOrderTraversal.cs
@@ -1,100 +1,90 @@
-//Complete the function to print spiral order traversal of a tree. For below tree, function should print 1, 2, 3, 4, 5, 6, 7.
+// C# implementation of an O(n) approach of 
+// level order traversal in spiral form 
+using System; 
+using System.Collections.Generic; 
 
+// A Binary Tree node 
 public class Node { 
-    public int data; 
-    public Node left, right; 
-  
-    public Node(int d) 
-    { 
-        data = d; 
-        left = right = null; 
-    } 
+	public int data; 
+	public Node left, right; 
+
+	public Node(int item) 
+	{ 
+		data = item; 
+		left = right = null; 
+	} 
 } 
-  
-class GFG { 
-    public Node root; 
-  
-    // Function to print the spiral 
-    // traversal of tree 
-    public virtual void printSpiral(Node node) 
-    { 
-        int h = height(node); 
-        int i; 
-  
-        /* ltr -> left to right. If this  
-        variable is set then the given 
-        label is traversed from left to right */
-        bool ltr = false; 
-        for (i = 1; i <= h; i++) { 
-            printGivenLevel(node, i, ltr); 
-  
-            /*Revert ltr to traverse next  
-              level in opposite order*/
-            ltr = !ltr; 
-        } 
-    } 
-  
-    /* Compute the "height" of a tree -- the 
-    number of nodes along the longest path  
-    from the root node down to the farthest  
-    leaf node.*/
-    public virtual int height(Node node) 
-    { 
-        if (node == null) { 
-            return 0; 
-        } 
-        else { 
-  
-            /* compute the height of each subtree */
-            int lheight = height(node.left); 
-            int rheight = height(node.right); 
-  
-            /* use the larger one */
-            if (lheight > rheight) { 
-                return (lheight + 1); 
-            } 
-            else { 
-                return (rheight + 1); 
-            } 
-        } 
-    } 
-  
-    /* Print nodes at a given level */
-    public virtual void printGivenLevel(Node node, 
-                                        int level, 
-                                        bool ltr) 
-    { 
-        if (node == null) { 
-            return; 
-        } 
-        if (level == 1) { 
-            Console.Write(node.data + " "); 
-        } 
-        else if (level > 1) { 
-            if (ltr != false) { 
-                printGivenLevel(node.left, level - 1, ltr); 
-                printGivenLevel(node.right, level - 1, ltr); 
-            } 
-            else { 
-                printGivenLevel(node.right, level - 1, ltr); 
-                printGivenLevel(node.left, level - 1, ltr); 
-            } 
-        } 
-    } 
-  
-    // Driver Code 
-    public static void Main(string[] args) 
-    { 
-        GFG tree = new GFG(); 
-        tree.root = new Node(1); 
-        tree.root.left = new Node(2); 
-        tree.root.right = new Node(3); 
-        tree.root.left.left = new Node(7); 
-        tree.root.left.right = new Node(6); 
-        tree.root.right.left = new Node(5); 
-        tree.root.right.right = new Node(4); 
-        Console.WriteLine("Spiral order traversal "
-                          + "of Binary Tree is "); 
-        tree.printSpiral(tree.root); 
-    } 
+
+public class BinaryTree { 
+	public static Node root; 
+
+	public virtual void printSpiral(Node node) 
+	{ 
+		if (node == null) { 
+			return; // NULL check 
+		} 
+
+		// Create two stacks to store alternate levels 
+		Stack<Node> s1 = new Stack<Node>(); // For levels to be printed 
+		// from right to left 
+		Stack<Node> s2 = new Stack<Node>(); // For levels to be printed 
+		// from left to right 
+
+		// Push first level to first stack 's1' 
+		s1.Push(node); 
+
+		// Keep printing while any of the 
+		// stacks has some nodes 
+		while (s1.Count > 0 || s2.Count > 0) { 
+			// Print nodes of current level from 
+			// s1 and push nodes of next level to s2 
+			while (s1.Count > 0) { 
+				Node temp = s1.Peek(); 
+				s1.Pop(); 
+				Console.Write(temp.data + " "); 
+
+				// Note that is right is pushed before left 
+				if (temp.right != null) { 
+					s2.Push(temp.right); 
+				} 
+
+				if (temp.left != null) { 
+					s2.Push(temp.left); 
+				} 
+			} 
+
+			// Print nodes of current level from s2 
+			// and push nodes of next level to s1 
+			while (s2.Count > 0) { 
+				Node temp = s2.Peek(); 
+				s2.Pop(); 
+				Console.Write(temp.data + " "); 
+
+				// Note that is left is pushed before right 
+				if (temp.left != null) { 
+					s1.Push(temp.left); 
+				} 
+				if (temp.right != null) { 
+					s1.Push(temp.right); 
+				} 
+			} 
+		} 
+	} 
+
+	// Driver Code 
+	public static void Main(string[] args) 
+	{ 
+		BinaryTree tree = new BinaryTree(); 
+		BinaryTree.root = new Node(1); 
+		BinaryTree.root.left = new Node(2); 
+		BinaryTree.root.right = new Node(3); 
+		BinaryTree.root.left.left = new Node(7); 
+		BinaryTree.root.left.right = new Node(6); 
+		BinaryTree.root.right.left = new Node(5); 
+		BinaryTree.root.right.right = new Node(4); 
+		Console.WriteLine("Spiral Order traversal of Binary Tree is "); 
+		tree.printSpiral(root); 
+	} 
 } 
+
+// This code is contributed by Shrikant13 
